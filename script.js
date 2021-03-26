a14b621839577b9bc9427aeec9de794ac4e4f235
shainasuri <suris546@newschool.edu>
2021-03-26 17:43:30 +0530

    second commit
    updated demo project page for mobile view

---
+++ script.js
@@ -0,0 +1,15 @@
+// things to add: alert box (temp) / curtain menu / dark/light mode 
+
+function alertWip() {
+    alert("This website is a work in progress. Stay tuned :) \n\n[Currently optimized for mobile view]");
+  }
+
+  function openNav() {
+    document.querySelector('#myNav').style.width = "100%";
+  }
+
+  function closeNav() {
+      document.querySelector('#myNav').style.width = "0%";
+  }
+
+//   reference: https://www.w3schools.com/howto/howto_js_curtain_menu.asp
